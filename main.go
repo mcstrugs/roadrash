4bd1db96c22b38d4c7953de9345ef587ee183105
Joe Martinson <joe.martinson@modalai.com>
2021-03-09 11:28:13 -0800

    example code

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/asciimoo/colly"
+)
 
 func main() {
     fmt.Println("Hello, World!")
+
+    c := colly.NewCollector(
+        colly.AllowedDomains("joemartinson.org"),
+    )
+
+    c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("https://joemartinson.org/")
 }
