1af090cd0e79957284da78781ae922637ad30609
Joe Martinson <joe.martinson@modalai.com>
2021-03-09 13:11:55 -0800

    initial newegg stock check

--- main.go
+++ main.go
@@ -2,23 +2,36 @@ package main
 
 import (
     "fmt"
+    "strings"
     "github.com/asciimoo/colly"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+    checkStock("https://www.newegg.com/yeston-rx560d-4g-d5-ga/p/27N-0042-00028")
+    checkStock("https://www.newegg.com/gigabyte-radeon-rx-6800-xt-gv-r68xtaorus-m-16gc/p/N82E16814932392")
+}
 
-    c := colly.NewCollector(
-        colly.AllowedDomains("joemartinson.org"),
-    )
+func checkStock(url string) bool {
+    in_stock := true
+    c := colly.NewCollector()
 
-    c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
+    c.OnHTML("span", func(e *colly.HTMLElement) {
+        if(strings.Contains(e.Text, "OUT OF STOCK")) {
+            in_stock = false
+        }
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.Visit("https://joemartinson.org/")
+	c.Visit(url)
+
+    if(in_stock) {
+        fmt.Println("IN STOCK")
+    } else {
+        fmt.Println("OUT OF STOCK")
+    }
+
+    return in_stock
 }
